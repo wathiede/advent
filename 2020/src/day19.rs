37fdc75db4d772dba291aefe10ae58a4cfd1f9fe
Bill Thiede <git@xinu.tv>
2020-12-19 15:16:10 -0800

    Shitty day 19 part 2 answer

--- 2020/src/day19.rs
+++ 2020/src/day19.rs
@@ -48,6 +48,85 @@
 //! Your goal is to determine the number of messages that completely match rule 0. In the above example, ababbb and abbbab match, but bababa, aaabbb, and aaaabbb do not, producing the answer 2. The whole message must match all of rule 0; there can't be extra unmatched characters in the message. (For example, aaaabbb might appear to match rule 0 above, but it has an extra unmatched b on the end.)
 //!
 //! How many messages completely match rule 0?
+//!
+//! --- Part Two ---
+//! As you look over the list of messages, you realize your matching rules aren't quite right. To fix them, completely replace rules 8: 42 and 11: 42 31 with the following:
+//!
+//! 8: 42 | 42 8
+//! 11: 42 31 | 42 11 31
+//! This small change has a big impact: now, the rules do contain loops, and the list of messages they could hypothetically match is infinite. You'll need to determine how these changes affect which messages are valid.
+//!
+//! Fortunately, many of the rules are unaffected by this change; it might help to start by looking at which rules always match the same set of values and how those rules (especially rules 42 and 31) are used by the new versions of rules 8 and 11.
+//!
+//! (Remember, you only need to handle the rules you have; building a solution that could handle any hypothetical combination of rules would be significantly more difficult.)
+//!
+//! For example:
+//!
+//! 42: 9 14 | 10 1
+//! 9: 14 27 | 1 26
+//! 10: 23 14 | 28 1
+//! 1: "a"
+//! 11: 42 31
+//! 5: 1 14 | 15 1
+//! 19: 14 1 | 14 14
+//! 12: 24 14 | 19 1
+//! 16: 15 1 | 14 14
+//! 31: 14 17 | 1 13
+//! 6: 14 14 | 1 14
+//! 2: 1 24 | 14 4
+//! 0: 8 11
+//! 13: 14 3 | 1 12
+//! 15: 1 | 14
+//! 17: 14 2 | 1 7
+//! 23: 25 1 | 22 14
+//! 28: 16 1
+//! 4: 1 1
+//! 20: 14 14 | 1 15
+//! 3: 5 14 | 16 1
+//! 27: 1 6 | 14 18
+//! 14: "b"
+//! 21: 14 1 | 1 14
+//! 25: 1 1 | 1 14
+//! 22: 14 14
+//! 8: 42
+//! 26: 14 22 | 1 20
+//! 18: 15 15
+//! 7: 14 5 | 1 21
+//! 24: 14 1
+//!
+//! abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+//! bbabbbbaabaabba
+//! babbbbaabbbbbabbbbbbaabaaabaaa
+//! aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+//! bbbbbbbaaaabbbbaaabbabaaa
+//! bbbababbbbaaaaaaaabbababaaababaabab
+//! ababaaaaaabaaab
+//! ababaaaaabbbaba
+//! baabbaaaabbaaaababbaababb
+//! abbbbabbbbaaaababbbbbbaaaababb
+//! aaaaabbaabaaaaababaa
+//! aaaabbaaaabbaaa
+//! aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+//! babaaabbbaaabaababbaabababaaab
+//! aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+//! Without updating rules 8 and 11, these rules only match three messages: bbabbbbaabaabba, ababaaaaaabaaab, and ababaaaaabbbaba.
+//!
+//! However, after updating rules 8 and 11, a total of 12 messages match:
+//!
+//! bbabbbbaabaabba
+//! babbbbaabbbbbabbbbbbaabaaabaaa
+//! aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+//! bbbbbbbaaaabbbbaaabbabaaa
+//! bbbababbbbaaaaaaaabbababaaababaabab
+//! ababaaaaaabaaab
+//! ababaaaaabbbaba
+//! baabbaaaabbaaaababbaababb
+//! abbbbabbbbaaaababbbbbbaaaababb
+//! aaaaabbaabaaaaababaa
+//! aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+//! aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+//! After updating rules 8 and 11, how many messages completely match rule 0?
+
 use std::collections::HashMap;
 use std::fmt;
 
@@ -88,6 +167,7 @@ struct Resolver {
 
 impl Resolver {
     fn resolve(&mut self, e: &Entry) -> String {
+        assert_ne!(e, &Entry::Rule(0));
         if let Some(v) = self.resolved.get(e) {
             return v.to_string();
         }
@@ -104,8 +184,10 @@ impl Resolver {
                     .clone()
                     .iter()
                     .map(|rule| {
-                        let letters: Vec<_> = rule.iter().map(|e| self.resolve(e)).collect();
-                        letters.join("")
+                        rule.iter()
+                            .map(|e| self.resolve(e))
+                            .collect::<Vec<_>>()
+                            .join("")
                     })
                     .collect();
                 let s = subrules.join("|");
@@ -121,14 +203,30 @@ impl Resolver {
 }
 
 fn expand_rulemap(rule_map: HashMap<usize, Vec<Vec<Entry>>>) -> Regex {
+    // Hack
+    let part2 = rule_map.len() > 8 && rule_map[&8].len() > 1;
     let mut r = Resolver {
         rule_map,
         resolved: HashMap::new(),
     };
-    let rule_zero = r.rule_map[&0][0].clone();
-    let re = rule_zero
-        .iter()
-        .fold("".to_string(), |acc, e| format!("{}{}", acc, r.resolve(e)));
+    let re = if part2 {
+        let hack = (1..10_usize)
+            .map(|i| {
+                format!(
+                    "{}{}",
+                    r.resolve(&Entry::Rule(42)).repeat(i),
+                    r.resolve(&Entry::Rule(31)).repeat(i)
+                )
+            })
+            .collect::<Vec<_>>()
+            .join("|");
+        format!("({})+({})", r.resolve(&Entry::Rule(42)), hack)
+    } else {
+        let rule_zero = r.rule_map[&0][0].clone();
+        rule_zero
+            .iter()
+            .fold("".to_string(), |acc, e| format!("{}{}", acc, r.resolve(e)))
+    };
 
     Regex::new(&format!(r"^{}$", re)).unwrap()
 }
@@ -146,13 +244,7 @@ fn make_rules(lines: Vec<String>) -> Regex {
                 sub.split(' ')
                     .map(|p| match p.parse() {
                         Ok(n) => Entry::Rule(n),
-                        Err(_) => Entry::Char(
-                            p.strip_prefix("\"")
-                                .unwrap()
-                                .strip_suffix("\"")
-                                .unwrap()
-                                .to_string(),
-                        ),
+                        Err(_) => Entry::Char(p[1..p.len() - 1].to_string()),
                     })
                     .collect()
             })
@@ -162,8 +254,8 @@ fn make_rules(lines: Vec<String>) -> Regex {
     expand_rulemap(rules)
 }
 
-#[aoc_generator(day19)]
-fn generator(input: &str) -> Input {
+#[aoc_generator(day19, part1)]
+fn generator_part1(input: &str) -> Input {
     let mut it = input.split("\n\n");
     let rules = make_rules(
         it.next()
@@ -191,10 +283,51 @@ fn solution1(input: &Input) -> usize {
         .count()
 }
 
+#[aoc_generator(day19, part2)]
+fn generator_part2(input: &str) -> Input {
+    let mut it = input.split("\n\n");
+    let rules = make_rules(
+        it.next()
+            .unwrap()
+            .split('\n')
+            .map(|s| s.trim())
+            .map(|s| {
+                if s.starts_with("8:") {
+                    return "8: 42 | 42 8";
+                }
+                if s.starts_with("11:") {
+                    return "11: 42 31 | 42 11 31";
+                }
+                s
+            })
+            .map(|s| s.to_string())
+            .collect(),
+    );
+
+    let messages = it
+        .next()
+        .unwrap()
+        .split('\n')
+        .map(|s| s.trim().to_string())
+        .collect();
+    Input { rules, messages }
+}
+
+#[aoc(day19, part2)]
+fn solution2(input: &Input) -> usize {
+    input
+        .messages
+        .iter()
+        .filter(|msg| input.rules.is_match(msg))
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
+    use pretty_assertions::assert_eq;
+
     use super::*;
-    const INPUT: &'static str = r#"0: 4 1 5
+    const INPUT1: &'static str = r#"0: 4 1 5
         1: 2 3 | 3 2
         2: 4 4 | 5 5
         3: 4 5 | 5 4
@@ -209,13 +342,13 @@ mod tests {
 
     #[test]
     fn part1() {
-        assert_eq!(solution1(&generator(INPUT)), 2);
+        assert_eq!(solution1(&generator_part1(INPUT1)), 2);
     }
 
     #[test]
-    fn parse() {
+    fn parse1() {
         assert_eq!(
-            generator(INPUT),
+            generator_part1(INPUT1),
             Input {
                 rules: Regex::new("^a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b$").unwrap(),
                 messages: vec!["ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb",]
@@ -241,5 +374,83 @@ mod tests {
             expand_rulemap(r).as_str(),
             Regex::new("^(aa|bb)$").unwrap().as_str()
         );
+    }
+    const INPUT2: &'static str = r#"42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"#;
+
+    #[test]
+    fn part2_matches() {
+        let input = generator_part2(INPUT2);
+        assert_eq!(
+            input
+                .messages
+                .iter()
+                .filter(|msg| input.rules.is_match(msg))
+                .collect::<Vec<_>>(),
+            vec![
+                "bbabbbbaabaabba",
+                "babbbbaabbbbbabbbbbbaabaaabaaa",
+                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+                "bbbbbbbaaaabbbbaaabbabaaa",
+                "bbbababbbbaaaaaaaabbababaaababaabab",
+                "ababaaaaaabaaab",
+                "ababaaaaabbbaba",
+                "baabbaaaabbaaaababbaababb",
+                "abbbbabbbbaaaababbbbbbaaaababb",
+                "aaaaabbaabaaaaababaa",
+                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
+            ]
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(solution2(&generator_part2(INPUT2)), 12);
     }
 }
