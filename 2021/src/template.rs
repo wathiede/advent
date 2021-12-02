ccc258bcb27eac1d8b9689287bb68a67a2eae774
Bill Thiede <git@xinu.tv>
2021-12-01 19:38:13 -0800

    Stub implementation template.

---
+++ 2021/src/template.rs
@@ -0,0 +1,34 @@
+#[aoc_generator(dayX)]
+fn parse(input: &str) -> Vec<u32> {
+    todo!("parse")
+}
+
+#[aoc(dayX, part1)]
+fn part1(depths: &[u32]) -> u32 {
+    todo!("part1")
+}
+
+/*
+#[aoc(dayX, part2)]
+fn part2(depths: &[u32]) -> u32 {
+    todo!("part2")
+}
+*/
+
+#[test]
+fn test_part1() {
+    let input = r#"
+"#
+    .trim();
+    assert_eq!(part1(&parse(input)), TODO);
+}
+
+/*
+#[test]
+fn test_part2() {
+    let input = r#"
+"#
+    .trim();
+    assert_eq!(part2(&parse(input)), TODO);
+}
+*/
