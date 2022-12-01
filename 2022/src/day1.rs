8c841820a815d8ab1ea1e242110d8d09edeeceb0
Bill Thiede <git@xinu.tv>
2022-12-01 08:58:44 -0800

    Revert "Faster day 1 part 2."
    This reverts commit 9bbcbef97d61fb155e137f19800bf14b8ac4b195.

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use aoc_runner_derive::aoc;
+use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc(day1, part1)]
 fn part1(input: &str) -> usize {
@@ -16,26 +16,14 @@ fn part1(input: &str) -> usize {
 
 #[aoc(day1, part2)]
 fn part2(input: &str) -> usize {
-    input
+    let mut cals: Vec<_> = input
         .split("\n\n")
         .map(|elf| {
             elf.split('\n')
                 .map(|cal| cal.parse::<usize>().expect("number"))
                 .sum()
         })
-        .fold([0; 3], |mut top, cal| {
-            if cal > top[0] {
-                top[0] = cal;
-                return top;
-            } else if cal > top[1] {
-                top[1] = cal;
-                return top;
-            } else if cal > top[2] {
-                top[2] = cal;
-                return top;
-            }
-            top
-        })
-        .iter()
-        .sum()
+        .collect();
+    cals.sort();
+    cals.iter().rev().take(3).sum()
 }
