ef68c931d2661e4a934ba0aa3d070b7607fb786e
Bill Thiede <git@xinu.tv>
2022-11-30 21:08:36 -0800

    Day 1 part 2.

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -13,3 +13,17 @@ fn part1(input: &str) -> usize {
         .max()
         .expect("max")
 }
+
+#[aoc(day1, part2)]
+fn part2(input: &str) -> usize {
+    let mut cals: Vec<_> = input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|cal| cal.parse::<usize>().expect("number"))
+                .sum()
+        })
+        .collect();
+    cals.sort();
+    cals.iter().rev().take(3).sum()
+}
