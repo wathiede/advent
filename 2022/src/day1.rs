c115ac1a11e35f2b655459dd45bf5f6e89e73ba5
Bill Thiede <git@xinu.tv>
2022-11-30 21:04:29 -0800

    Day 1 part 1.

--- 2022/src/day1.rs
+++ 2022/src/day1.rs
@@ -3,5 +3,13 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc(day1, part1)]
 fn part1(input: &str) -> usize {
-    0
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.split('\n')
+                .map(|cal| cal.parse::<usize>().expect("number"))
+                .sum()
+        })
+        .max()
+        .expect("max")
 }
