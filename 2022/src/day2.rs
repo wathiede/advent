0cf21df33dd7a2064cfebbde5f7d9b4dfca1e69c
Bill Thiede <git@xinu.tv>
2022-12-01 21:24:24 -0800

    Day 2 part 2.

--- 2022/src/day2.rs
+++ 2022/src/day2.rs
@@ -26,6 +26,26 @@ impl Play {
             (Paper, Rock) | (Scissors, Paper) | (Rock, Scissors) => 6,
         }
     }
+    fn solve(&self, s: &str) -> Play {
+        use Play::*;
+        // X lose
+        // Y draw
+        // Z win
+        match (self, s) {
+            (&Rock, "X") => Play::Scissors,
+            (&Rock, "Y") => Play::Rock,
+            (&Rock, "Z") => Play::Paper,
+
+            (&Scissors, "X") => Play::Paper,
+            (&Scissors, "Y") => Play::Scissors,
+            (&Scissors, "Z") => Play::Rock,
+
+            (&Paper, "X") => Play::Rock,
+            (&Paper, "Y") => Play::Paper,
+            (&Paper, "Z") => Play::Scissors,
+            (&Rock, _) | (&Scissors, _) | (&Paper, _) => panic!("Unknown play '{}'", s),
+        }
+    }
 }
 
 impl FromStr for Play {
@@ -57,5 +77,13 @@ fn part1(input: &str) -> usize {
 
 #[aoc(day2, part2)]
 fn part2(input: &str) -> usize {
-    0
+    input
+        .split("\n")
+        .map(|l| {
+            let (them, me) = l.split_once(' ').unwrap();
+            let them: Play = them.parse().expect("couldn't parse them");
+            let me = them.solve(me);
+            me.play(&them) + me.points()
+        })
+        .sum()
 }
