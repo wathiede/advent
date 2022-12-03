a8c9e97f37cd74236b55f9e380f4f02dc7cab134
Bill Thiede <git@xinu.tv>
2022-12-02 21:26:11 -0800

    Day 3 part 2.

--- 2022/src/day3.rs
+++ 2022/src/day3.rs
@@ -23,5 +23,32 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
-// #[aoc(day3, part2)]
-// fn part2(input: &str) -> usize { }
+#[aoc(day3, part2)]
+fn part2(input: &str) -> usize {
+    let lines: Vec<_> = input.split('\n').collect();
+
+    lines
+        .chunks(3)
+        .map(|lines| {
+            let a = lines[0];
+            let b = lines[1];
+            let c = lines[2];
+            let r1: HashSet<_> = a.as_bytes().into_iter().collect();
+            let r2: HashSet<_> = b.as_bytes().into_iter().collect();
+            let r3: HashSet<_> = c.as_bytes().into_iter().collect();
+            let m = *r1
+                .intersection(&r2)
+                .map(|u| *u)
+                .collect::<HashSet<&u8>>()
+                .intersection(&r3)
+                .next()
+                .unwrap();
+            let b = if m >= &b'a' {
+                m - &b'a' + 1
+            } else {
+                m - &b'A' + 27
+            };
+            b as usize
+        })
+        .sum()
+}
