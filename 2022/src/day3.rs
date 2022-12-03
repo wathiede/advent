95782ef8da9ed8dd4f0c1cac4eddfdcf9b90e1eb
Bill Thiede <git@xinu.tv>
2022-12-02 21:15:46 -0800

    Day 3 part 1.

--- 2022/src/day3.rs
+++ 2022/src/day3.rs
@@ -1,11 +1,26 @@
-use std::str::FromStr;
+use std::collections::HashSet;
 
 use anyhow::Result;
 use aoc_runner_derive::aoc;
 
 #[aoc(day3, part1)]
 fn part1(input: &str) -> usize {
-    0
+    input
+        .split('\n')
+        .map(|l| {
+            let l = l.as_bytes();
+            let h = l.len() / 2;
+            let r1: HashSet<_> = l[..h].iter().collect();
+            let r2: HashSet<_> = l[h..].iter().collect();
+            let m = *r1.intersection(&r2).next().unwrap();
+            let b = if m >= &b'a' {
+                m - &b'a' + 1
+            } else {
+                m - &b'A' + 27
+            };
+            b as usize
+        })
+        .sum()
 }
 
 // #[aoc(day3, part2)]
