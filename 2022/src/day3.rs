bef6c4edaa49b853ae2ea8daa75301fc2319f3f8
Bill Thiede <git@xinu.tv>
2022-12-03 09:21:37 -0800

    Day 3 part 2, bench reports -93.422% improvement.

--- 2022/src/day3.rs
+++ 2022/src/day3.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use anyhow::Result;
 use aoc_runner_derive::aoc;
 
@@ -21,43 +19,39 @@ fn part1(input: &str) -> u32 {
             let h = l.len() / 2;
             let r1 = l[..h].iter().fold(0_u64, |bits, b| bits | char_to_bit(*b));
             let r2 = l[h..].iter().fold(0_u64, |bits, b| bits | char_to_bit(*b));
-            let union = r1 & r2;
-            debug_assert_eq!(1, union.count_zeros());
-            union.trailing_zeros()
+            let intersection = r1 & r2;
+            debug_assert_eq!(1, intersection.count_zeros());
+            intersection.trailing_zeros()
         })
         .sum();
-    assert_eq!(answer, 8018);
+    debug_assert_eq!(answer, 8018);
     answer
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &str) -> usize {
+fn part2(input: &str) -> u32 {
     let lines: Vec<_> = input.split('\n').collect();
 
     let answer = lines
         .chunks(3)
         .map(|lines| {
-            let a = lines[0];
-            let b = lines[1];
-            let c = lines[2];
-            let r1: HashSet<_> = a.as_bytes().into_iter().collect();
-            let r2: HashSet<_> = b.as_bytes().into_iter().collect();
-            let r3: HashSet<_> = c.as_bytes().into_iter().collect();
-            let m = *r1
-                .intersection(&r2)
-                .map(|u| *u)
-                .collect::<HashSet<&u8>>()
-                .intersection(&r3)
-                .next()
-                .unwrap();
-            let b = if m >= &b'a' {
-                m - &b'a' + 1
-            } else {
-                m - &b'A' + 27
-            };
-            b as usize
+            let a = lines[0]
+                .as_bytes()
+                .iter()
+                .fold(0_u64, |bits, b| bits | char_to_bit(*b));
+            let b = lines[1]
+                .as_bytes()
+                .iter()
+                .fold(0_u64, |bits, b| bits | char_to_bit(*b));
+            let c = lines[2]
+                .as_bytes()
+                .iter()
+                .fold(0_u64, |bits, b| bits | char_to_bit(*b));
+            let intersection = a & b & c;
+            debug_assert_eq!(1, intersection.count_zeros());
+            intersection.trailing_zeros()
         })
         .sum();
-    assert_eq!(answer, 2518);
+    debug_assert_eq!(answer, 2518);
     answer
 }
