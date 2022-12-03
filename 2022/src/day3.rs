1ce73e975aa3bb347690625d140b2433ce86d6cc
Bill Thiede <git@xinu.tv>
2022-12-03 09:16:52 -0800

    Day 3 part 1, bench reports a -92.139% improvement.

--- 2022/src/day3.rs
+++ 2022/src/day3.rs
@@ -3,31 +3,38 @@ use std::collections::HashSet;
 use anyhow::Result;
 use aoc_runner_derive::aoc;
 
+fn char_to_bit(c: u8) -> u64 {
+    let bit = if c >= b'a' {
+        c - b'a' + 1
+    } else {
+        c - b'A' + 27
+    };
+    1 << bit
+}
+
 #[aoc(day3, part1)]
-fn part1(input: &str) -> usize {
-    input
+fn part1(input: &str) -> u32 {
+    let answer = input
         .split('\n')
         .map(|l| {
             let l = l.as_bytes();
             let h = l.len() / 2;
-            let r1: HashSet<_> = l[..h].iter().collect();
-            let r2: HashSet<_> = l[h..].iter().collect();
-            let m = *r1.intersection(&r2).next().unwrap();
-            let b = if m >= &b'a' {
-                m - &b'a' + 1
-            } else {
-                m - &b'A' + 27
-            };
-            b as usize
+            let r1 = l[..h].iter().fold(0_u64, |bits, b| bits | char_to_bit(*b));
+            let r2 = l[h..].iter().fold(0_u64, |bits, b| bits | char_to_bit(*b));
+            let union = r1 & r2;
+            debug_assert_eq!(1, union.count_zeros());
+            union.trailing_zeros()
         })
-        .sum()
+        .sum();
+    assert_eq!(answer, 8018);
+    answer
 }
 
 #[aoc(day3, part2)]
 fn part2(input: &str) -> usize {
     let lines: Vec<_> = input.split('\n').collect();
 
-    lines
+    let answer = lines
         .chunks(3)
         .map(|lines| {
             let a = lines[0];
@@ -50,5 +57,7 @@ fn part2(input: &str) -> usize {
             };
             b as usize
         })
-        .sum()
+        .sum();
+    assert_eq!(answer, 2518);
+    answer
 }
