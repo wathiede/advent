7caba20c7834916b902c3755b5001fb647ccb47d
Bill Thiede <git@xinu.tv>
2022-12-03 21:13:49 -0800

    Day 4 part 2.

--- 2022/src/day4.rs
+++ 2022/src/day4.rs
@@ -27,11 +27,39 @@ fn part1(input: &str) -> usize {
             (e1l <= e2l && e1h >= e2h) || (e2l <= e1l && e2h >= e1h)
         })
         .count()
-    // Not 18
 }
 
-// #[aoc(day4, part2)]
-// fn part2(input: &str) -> usize { }
+#[aoc(day4, part2)]
+fn part2(input: &str) -> usize {
+    input
+        .split('\n')
+        .filter(|l| {
+            let (e1, e2) = l.split_once(',').unwrap();
+            let (e1l, e1h) = e1
+                .split_once('-')
+                .map(|(l, h)| {
+                    (
+                        l.parse::<usize>().expect("not a number"),
+                        h.parse::<usize>().expect("not a number"),
+                    )
+                })
+                .unwrap();
+            let (e2l, e2h) = e2
+                .split_once('-')
+                .map(|(l, h)| {
+                    (
+                        l.parse::<usize>().expect("not a number"),
+                        h.parse::<usize>().expect("not a number"),
+                    )
+                })
+                .unwrap();
+            (e2l <= e1l && e1l <= e2h)
+                || (e2l <= e1h && e1h <= e2h)
+                || (e1l <= e2l && e2l <= e1h)
+                || (e1l <= e2h && e2h <= e1h)
+        })
+        .count()
+}
 
 #[test]
 fn test1() {
