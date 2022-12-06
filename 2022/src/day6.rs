f868bafa919c879da8815a0c10844113d5ba23d1
Bill Thiede <git@xinu.tv>
2022-12-05 21:11:33 -0800

    Day 6 part 1.

--- 2022/src/day6.rs
+++ 2022/src/day6.rs
@@ -1,9 +1,22 @@
+use std::collections::HashSet;
+
 use aoc_runner_derive::aoc;
 
 #[aoc(day6, part1)]
 fn part1(input: &str) -> usize {
+    for (i, chars) in input.as_bytes().windows(4).enumerate() {
+        let uniq: HashSet<_> = chars.iter().collect();
+        dbg!(&chars, &uniq);
+        if uniq.len() == 4 {
+            return i + 4;
+        }
+    }
     0
 }
 
+#[test]
+fn p1() {
+    assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+}
 // #[aoc(day6, part2)]
 // fn part2(input: &str) -> usize { }
