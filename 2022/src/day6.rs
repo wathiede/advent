fd836ec76e8664d4fff334337a9256f587fbcfe0
Bill Thiede <git@xinu.tv>
2022-12-06 19:35:10 -0800

    Day 6 one function for both parts.

--- 2022/src/day6.rs
+++ 2022/src/day6.rs
@@ -2,21 +2,21 @@ use std::collections::HashSet;
 
 use aoc_runner_derive::aoc;
 
+fn solve(input: &str, marker_len: usize) -> usize {
+    input
+        .as_bytes()
+        .windows(marker_len)
+        .position(|chars| {
+            let uniq: HashSet<_> = chars.iter().collect();
+            uniq.len() == marker_len
+        })
+        .expect("not found")
+        + marker_len
+}
+
 #[aoc(day6, part1)]
 fn part1(input: &str) -> usize {
-    let marker_len = 4;
-    let v = {
-        input
-            .as_bytes()
-            .windows(marker_len)
-            .position(|chars| {
-                let uniq: HashSet<_> = chars.iter().collect();
-                uniq.len() == 4
-            })
-            .expect("not found")
-            + marker_len
-    };
-    v
+    solve(input, 4)
 }
 
 #[test]
@@ -25,16 +25,7 @@ fn p1() {
 }
 #[aoc(day6, part2)]
 fn part2(input: &str) -> usize {
-    let v = {
-        let marker_len = 14;
-        for (i, chars) in input.as_bytes().windows(marker_len).enumerate() {
-            let uniq: HashSet<_> = chars.iter().collect();
-            if uniq.len() == marker_len {
-                return i + marker_len;
-            }
-        }
-        0
-    };
+    let v = solve(input, 14);
     assert_eq!(v, 3380);
     v
 }
