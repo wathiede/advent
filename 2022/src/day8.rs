c6434d6faf7760c3a3e826d6d6d28b418a3f87ea
Bill Thiede <git@xinu.tv>
2022-12-07 23:22:05 -0800

    Day 8 part 2.

--- 2022/src/day8.rs
+++ 2022/src/day8.rs
@@ -17,12 +17,12 @@ fn part1(input: &str) -> usize {
             // From left
             let h = m[y][x];
             let viz = (0..x).all(|x1| m[y][x1]<h)
-    // From right
-    ||(x+1..m.len()).all(|x1| m[y][x1]<h)
-    // From top 
-    ||(0..y).all(|y1| m[y1][x]<h)
-    // From bottom
-    ||(y+1..m.len()).all(|y1| m[y1][x]<h);
+                // From right
+                ||(x+1..m.len()).all(|x1| m[y][x1]<h)
+                // From top
+                ||(0..y).all(|y1| m[y1][x]<h)
+                // From bottom
+                ||(y+1..m.len()).all(|y1| m[y1][x]<h);
 
             if viz {
                 trees[y] |= 1 << x
@@ -34,8 +34,88 @@ fn part1(input: &str) -> usize {
     trees.iter().map(|r| r.count_ones() as usize).sum::<usize>()
 }
 
-// #[aoc(day8, part2)]
-// fn part2(input: &str) -> usize { }
+#[aoc(day8, part2)]
+fn part2(input: &str) -> usize {
+    let m: Vec<Vec<u8>> = input
+        .lines()
+        .map(|l| l.as_bytes().iter().map(|b| b - b'0').collect())
+        .collect();
+    let mut scores: Vec<Vec<usize>> = m.iter().map(|r| vec![0; r.len()]).collect();
+    let mut up: Vec<Vec<usize>> = m.iter().map(|r| vec![0; r.len()]).collect();
+    let mut left: Vec<Vec<usize>> = m.iter().map(|r| vec![0; r.len()]).collect();
+    let mut right: Vec<Vec<usize>> = m.iter().map(|r| vec![0; r.len()]).collect();
+    let mut down: Vec<Vec<usize>> = m.iter().map(|r| vec![0; r.len()]).collect();
+    for y in 0..m.len() {
+        for x in 0..m.len() {
+            let h = m[y][x];
+            // Up
+            up[y][x] = (0..y)
+                .rev()
+                .position(|y1| m[y1][x] >= h)
+                .map(|v| v + 1)
+                .unwrap_or(y);
+            // Left
+            left[y][x] = (0..x)
+                .rev()
+                .position(|x1| m[y][x1] >= h)
+                .map(|v| v + 1)
+                .unwrap_or(x);
+            // Right
+            right[y][x] = (x + 1..m.len())
+                .position(|x1| m[y][x1] >= h)
+                .map(|v| v + 1)
+                .unwrap_or(m.len() - x - 1);
+            // Down
+            down[y][x] = (y + 1..m.len())
+                .position(|y1| m[y1][x] >= h)
+                .map(|v| v + 1)
+                .unwrap_or(m.len() - y - 1);
+
+            scores[y][x] = up[y][x] * left[y][x] * right[y][x] * down[y][x];
+        }
+    }
+    // println!("Trees");
+    // m.iter().for_each(|r| {
+    //     r.iter().for_each(|v| print!("{v:4}"));
+    //     println!();
+    // });
+
+    //  println!("Up");
+    //  up.iter().for_each(|r| {
+    //      r.iter().for_each(|v| print!("{v:4}"));
+    //      println!();
+    //  });
+    // println!("Left");
+    // left.iter().for_each(|r| {
+    //     r.iter().for_each(|v| print!("{v:4}"));
+    //     println!();
+    // });
+    // println!("Right");
+    // right.iter().for_each(|r| {
+    //     r.iter().for_each(|v| print!("{v:4}"));
+    //     println!();
+    // });
+    // println!("Down");
+    // down.iter().for_each(|r| {
+    //     r.iter().for_each(|v| print!("{v:4}"));
+    //     println!();
+    // });
+
+    // println!("Scores");
+    // scores.iter().for_each(|r| {
+    //     r.iter().for_each(|v| print!("{v:4}"));
+    //     println!();
+    // });
+    // assert_eq!(scores[1][2], 4);
+    // assert_eq!(scores[3][2], 8);
+    // Not 1716
+    scores
+        .into_iter()
+        .map(|r| r.into_iter().max().unwrap())
+        .max()
+        .unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,5 +128,15 @@ mod tests {
     #[test]
     fn p1() {
         assert_eq!(part1(INPUT1), 21);
+    }
+    const INPUT2: &'static str = r#"30373
+25512
+65332
+33549
+35390"#;
+
+    #[test]
+    fn p2() {
+        assert_eq!(part2(INPUT2), 8);
     }
 }
