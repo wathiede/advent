a7f58388d2929c98653336a8625adf81bdb6a850
Bill Thiede <git@xinu.tv>
2022-12-07 22:03:00 -0800

    Day 8 part 1

--- 2022/src/day8.rs
+++ 2022/src/day8.rs
@@ -2,8 +2,51 @@ use aoc_runner_derive::aoc;
 
 #[aoc(day8, part1)]
 fn part1(input: &str) -> usize {
-    0
+    let m: Vec<&[u8]> = input.lines().map(|l| l.as_bytes()).collect();
+    let mut trees: Vec<u128> = (0..m.len())
+        .map(|i| {
+            if i == 0 || i == m.len() - 1 {
+                (1 << m.len()) - 1
+            } else {
+                1 | (1 << m.len() - 1)
+            }
+        })
+        .collect();
+    for y in 1..m.len() - 1 {
+        for x in 1..m.len() - 1 {
+            // From left
+            let h = m[y][x];
+            let viz = (0..x).all(|x1| m[y][x1]<h)
+    // From right
+    ||(x+1..m.len()).all(|x1| m[y][x1]<h)
+    // From top 
+    ||(0..y).all(|y1| m[y1][x]<h)
+    // From bottom
+    ||(y+1..m.len()).all(|y1| m[y1][x]<h);
+
+            if viz {
+                trees[y] |= 1 << x
+            }
+        }
+    }
+    //let w = m.len();
+    //trees.iter().for_each(|r| println!("{r:w$b}"));
+    trees.iter().map(|r| r.count_ones() as usize).sum::<usize>()
 }
 
 // #[aoc(day8, part2)]
 // fn part2(input: &str) -> usize { }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT1: &'static str = r#"30373
+25512
+65332
+33549
+35390"#;
+
+    #[test]
+    fn p1() {
+        assert_eq!(part1(INPUT1), 21);
+    }
+}
