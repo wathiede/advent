a4ac2dfb1d095ce614d712f4ee767e62a39b6c92
Bill Thiede <git@xinu.tv>
2022-12-08 23:17:47 -0800

    Day 9 part 2.

--- 2022/src/day9.rs
+++ 2022/src/day9.rs
@@ -2,24 +2,29 @@ use std::{collections::HashSet, fmt};
 
 use aoc_runner_derive::aoc;
 
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 struct Pos {
     x: isize,
     y: isize,
 }
 
-#[derive(Default)]
 struct Grid {
-    t: Pos,
-    h: Pos,
+    knots: Vec<Pos>,
     places: HashSet<(isize, isize)>,
 }
 
 impl Grid {
+    fn new(num_knots: usize) -> Grid {
+        Grid {
+            knots: (0..num_knots).map(|_| Pos::default()).collect(),
+            places: HashSet::default(),
+        }
+    }
     fn places(&self) -> usize {
         self.places.len()
     }
     fn walk(&mut self, d: &str, n: isize) {
+        //println!("== {} {} ==", d, n);
         for _ in 0..n {
             self.step(d);
         }
@@ -27,33 +32,51 @@ impl Grid {
     fn step(&mut self, d: &str) {
         // Move head.
         match d {
-            "R" => self.h.x += 1,
-            "L" => self.h.x -= 1,
-            "U" => self.h.y -= 1,
-            "D" => self.h.y += 1,
+            "R" => self.knots[0].x += 1,
+            "L" => self.knots[0].x -= 1,
+            "U" => self.knots[0].y -= 1,
+            "D" => self.knots[0].y += 1,
             _ => panic!("unexpected dir"),
         }
-        let x_d = self.h.x - self.t.x;
-        let y_d = self.h.y - self.t.y;
-        //dbg!(&self.h, &self.t, x_d, y_d);
-        if x_d.abs() > 1 {
-            self.t.x += x_d.signum();
-            if y_d != 0 {
-                self.t.y = self.h.y;
+        //dbg!(&self.knots);
+        for i in 1..self.knots.len() {
+            let h = &self.knots[i - 1];
+            let mut t = self.knots[i].clone();
+            let x_d = h.x - t.x;
+            let y_d = h.y - t.y;
+            //println!("{}: {:?} {:?} {:?} {:?}", i - 1, &h, &t, x_d, y_d);
+            // 2,-2 + 0,0 => 1,-1
+            // d=2,-2
+            if x_d.abs() > 1 && y_d.abs() > 1 {
+                t.x += x_d.signum();
+                t.y += y_d.signum();
+            } else {
+                if x_d.abs() > 1 {
+                    t.x += x_d.signum();
+                    if y_d != 0 {
+                        t.y = h.y;
+                    }
+                }
+                if y_d.abs() > 1 {
+                    t.y += y_d.signum();
+                    if x_d != 0 {
+                        t.x = h.x;
+                    }
+                }
             }
-        }
-        if y_d.abs() > 1 {
-            self.t.y += y_d.signum();
-            if x_d != 0 {
-                self.t.x = self.h.x;
+            // Only update the last knot's location.
+            //dbg!(i, self.knots.len(), i == self.knots.len());
+            if i == self.knots.len() - 1 {
+                self.places.insert((t.x, t.y));
             }
+            //println!("s: {} {:?}", i, &t);
+            debug_assert!((h.x - t.x).abs() <= 1);
+            debug_assert!((h.y - t.y).abs() <= 1);
+            self.knots[i] = t;
         }
-        self.places.insert((self.t.x, self.t.y));
+        //println!("Step:\n{}", &self);
     }
-}
-
-impl fmt::Display for Grid {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    fn fmt_visits(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         use std::cmp::{max, min};
         let (x_min, x_max, y_min, y_max) = self.places.iter().fold(
             (isize::MAX, isize::MIN, isize::MAX, isize::MIN),
@@ -62,8 +85,35 @@ impl fmt::Display for Grid {
         //dbg!((x_min, x_max, y_min, y_max));
         for y in y_min - 2..y_max + 2 {
             for x in x_min - 2..x_max + 2 {
-                if self.places.contains(&(x, y)) {
+                if x == 0 && y == 0 {
+                    write!(f, "s")?;
+                } else if self.places.contains(&(x, y)) {
                     write!(f, "X")?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+    fn fmt_rope(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use std::cmp::{max, min};
+        let (x_min, x_max, y_min, y_max) = self.knots.iter().map(|p| (p.x, p.y)).fold(
+            (isize::MAX, isize::MIN, isize::MAX, isize::MIN),
+            |(xi, xa, yi, ya), (x, y)| (min(x, xi), max(x, xa), min(y, yi), max(y, ya)),
+        );
+        //dbg!((x_min, x_max, y_min, y_max));
+        for y in y_min - 2..y_max + 2 {
+            for x in x_min - 2..x_max + 2 {
+                if let Some(p) = self.knots.iter().position(|k| k.x == x && k.y == y) {
+                    if p == 0 {
+                        write!(f, "H")?;
+                    } else {
+                        write!(f, "{}", p)?;
+                    }
+                } else if x == 0 && y == 0 {
+                    write!(f, "s")?;
                 } else {
                     write!(f, ".")?;
                 }
@@ -74,9 +124,15 @@ impl fmt::Display for Grid {
     }
 }
 
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.fmt_rope(f)
+    }
+}
+
 #[aoc(day9, part1)]
 fn part1(input: &str) -> usize {
-    let mut g = Grid::default();
+    let mut g = Grid::new(2);
     input
         .lines()
         .map(|l| {
@@ -86,7 +142,7 @@ fn part1(input: &str) -> usize {
         })
         .for_each(|(d, n)| {
             g.walk(d, n);
-            //println!("{} {}\n{}", d, n, g);
+            //println!("Walk\n{} {}\n{}", d, n, g);
         });
     g.places()
 }
@@ -104,5 +160,40 @@ R 2"#;
     assert_eq!(part1(s), 13);
 }
 
-// #[aoc(day9, part2)]
-// fn part2(input: &str) -> usize { }
+#[aoc(day9, part2)]
+fn part2(input: &str) -> usize {
+    let mut g = Grid::new(10);
+    input
+        .lines()
+        .map(|l| {
+            l.split_once(' ')
+                .map(|(d, n)| (d, n.parse::<isize>().expect("couldn't parse number")))
+                .expect("couldn't split line")
+        })
+        .for_each(|(d, n)| {
+            g.walk(d, n);
+        });
+    g.places()
+}
+
+#[test]
+fn p2() {
+    let s1 = r#"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"#;
+    assert_eq!(part2(s1), 1);
+    let s2 = r#"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"#;
+    assert_eq!(part2(s2), 36);
+}
