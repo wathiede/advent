0a2aa4511d7672adfd5b50e98b4c17d4f3a5852b
Bill Thiede <git@xinu.tv>
2022-12-11 11:08:35 -0800

    Update stubs to include tests.

--- 2022/src/day12.rs
+++ 2022/src/day12.rs
@@ -7,3 +7,20 @@ fn part1(input: &str) -> usize {
 
 // #[aoc(day12, part2)]
 // fn part2(input: &str) -> usize { }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &'static str = r#"SOMETHING
+"#;
+
+    #[test]
+    fn p1() {
+        assert_eq!(part1(INPUT), 42);
+    }
+
+    //#[test]
+    //fn p2() {
+    //    assert_eq!(part2(INPUT), 42);
+    //}
+}
