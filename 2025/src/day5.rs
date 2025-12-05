f232ea259cd3e2bc428a9cfe3703e39b57a7914e
Bill Thiede <git@xinu.tv>
2025-12-04 21:21:54 -0800

    2025: day 5 part 2

--- 2025/src/day5.rs
+++ 2025/src/day5.rs
@@ -1,3 +1,4 @@
+use advent::prelude::*;
 use aoc_runner_derive::{aoc, aoc_generator};
 #[aoc_generator(day5)]
 fn parse(input: &str) -> String {
@@ -30,9 +31,42 @@ fn part1(input: &str) -> String {
     cnt.to_string()
 }
 
+fn merge_ranges(mut ranges: Vec<RangeInclusive<u64>>) -> Vec<RangeInclusive<u64>> {
+    let mut mr: Vec<RangeInclusive<u64>> = Vec::new();
+    ranges.sort_by_key(|r| *r.start());
+    for r in ranges {
+        if let Some(last) = mr.last_mut() {
+            if last.contains(r.start()) {
+                *last = *last.start()..=*last.end().max(r.end());
+            } else {
+                mr.push(r);
+            }
+        } else {
+            mr.push(r);
+        }
+    }
+    mr
+}
+
 #[aoc(day5, part2)]
 fn part2(input: &str) -> String {
-    todo!()
+    let mut it = input.lines();
+    let mut ranges = Vec::new();
+    loop {
+        let l = it.next().expect("failed to get db line");
+        if l.is_empty() {
+            break;
+        }
+
+        let (lo, hi) = l.split_once('-').expect("failed to split -");
+        ranges.push(lo.parse::<u64>().expect("lo failed")..=hi.parse::<u64>().expect("hi failed"));
+    }
+    let ranges = merge_ranges(ranges);
+    let mut cnt = 0;
+    for r in ranges {
+        cnt += r.end() - r.start() + 1;
+    }
+    cnt.to_string()
 }
 
 #[cfg(test)]
@@ -55,11 +89,9 @@ mod tests {
         assert_eq!(part1(&parse(INPUT)), "3");
     }
 
-    /*
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(INPUT)), "14");
     }
-    */
 }
 
