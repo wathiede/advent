adc1d6cca148cdd7eaea61b439fea4d427575750
Bill Thiede <git@xinu.tv>
2025-12-06 21:43:53 -0800

    2025: day 7 part 2 test pass, real input too slow

--- 2025/src/day7.rs
+++ 2025/src/day7.rs
@@ -32,9 +32,52 @@ fn part1(im: &Image) -> String {
     splits.to_string()
 }
 
+fn rays_as_bits(rays: &[bool]) -> String {
+    rays.iter()
+        .map(|r| if *r { "|" } else { "." })
+        .collect::<Vec<_>>()
+        .join("")
+}
+// TODO: bitset
+// TODO: remove cloned
+fn explore(im: &Image, y: usize, mut rays: &mut [bool], mut timelines: &mut usize) {
+    //dbg!(y, rays_as_bits(rays), &timelines);
+    if y >= im.height {
+        return;
+    }
+
+    for x in 0..im.width {
+        if rays[x] {
+            if im[(x, y)] == b'^' {
+                // Split
+                rays[x] = false;
+                rays[x - 1] = true;
+                *timelines += 1;
+                explore(im, y + 1, &mut rays, &mut timelines);
+                rays[x - 1] = false;
+                rays[x + 1] = true;
+                explore(im, y + 1, &mut rays, &mut timelines);
+                rays[x + 1] = false;
+            } else {
+                explore(im, y + 1, &mut rays, &mut timelines);
+            }
+        }
+    }
+}
+
 #[aoc(day7, part2)]
 fn part2(im: &Image) -> String {
-    todo!()
+    let mut rays = vec![false; im.width];
+    for x in 0..im.width {
+        if im[(x, 0)] == b'S' {
+            rays[x] = true;
+            break;
+        }
+    }
+    let y_start = 1;
+    let mut timelines = 1;
+    explore(im, y_start, &mut rays, &mut timelines);
+    timelines.to_string()
 }
 
 #[cfg(test)]
@@ -64,11 +107,9 @@ mod tests {
         // 1555
     }
 
-    /*
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(INPUT)), "40");
     }
-    */
 }
 
