73b90d381956592258f6991e1dbc3185804ca002
Bill Thiede <git@xinu.tv>
2025-12-09 20:46:08 -0800

    2025: day 9 part 2 experimenting

--- 2025/src/day9.rs
+++ 2025/src/day9.rs
@@ -54,8 +54,42 @@ fn part1_min_max(input: &[(isize, isize)]) -> String {
     .to_string()
 }
 
+fn draw_line(im: &mut Image<char>, p1: (isize, isize), p2: (isize, isize)) {
+    let x_min = p1.0.min(p2.0) as usize;
+    let x_max = 1 + p1.0.max(p2.0) as usize;
+    let y_min = p1.1.min(p2.1) as usize;
+    let y_max = 1 + p1.1.max(p2.1) as usize;
+    println!("draw_line {x_min}..{x_max} {y_min}..{y_max}");
+    for y in y_min..y_max {
+        for x in x_min..x_max {
+            if im[(x, y)] != '#' {
+                im[(x, y)] = 'X';
+            }
+        }
+    }
+}
+
+fn draw_points(pts: &[(isize, isize)]) {
+    let (max_x, max_y) = pts.iter().fold((0isize, 0isize), |acc, (x, y)| {
+        (acc.0.max(*x), acc.1.max(*y))
+    });
+    println!("image {max_x}x{max_y}");
+    let mut im = Image::new(2 + max_x as usize, 2 + max_y as usize, '.');
+    im[(pts[0].0 as usize, pts[0].1 as usize)] = '#';
+    println!("{im:#}");
+    for idx in 1..pts.len() + 1 {
+        draw_line(&mut im, pts[idx - 1], pts[idx % pts.len()]);
+        let pt = pts[idx % pts.len()];
+        im[(pt.0 as usize, pt.1 as usize)] = '#';
+        println!("{im:#}");
+    }
+}
+
 #[aoc(day9, part2)]
 fn part2(input: &[(isize, isize)]) -> String {
+    draw_points(input);
+    // Walk points finding the nearest X neighbor and completing the rectangle
+    //
     todo!()
 }
 
@@ -83,4 +117,3 @@ mod tests {
         assert_eq!(part2(&parse(INPUT)), "24");
     }
 }
-
